9f242d4e0c665d09f51d151be92ec982db830a52
Alex Corvi <alex@arrayy.com>
2017-01-24 08:41:01 +0300

    updated and added tests

--- test.js
+++ test.js
@@ -1,71 +1,82 @@
-const langr = require('./index.js');
+const fin = require('./index.js');
 const assert = require('assert');
 
 
 describe('Basic checks', function () {
 
 	var input = "This is a sentence";
-	var instance = new langr(input);
+	var instance = new fin(input);
 	it('instance is an object', function () {
 		assert.equal(typeof instance.hasOwnProperty,"function");
 	});
 	it('input is a string', function () {
 		assert.equal(instance.input,input);
 	});
-	it('output is an array', function () {
-		assert.equal(Array.isArray(instance.output),true);
+	it('result is an array', function () {
+		assert.equal(Array.isArray(instance.result),true);
 	});
 	it('sentences is an array', function () {
 		assert.equal(Array.isArray(instance.sentences),true);
 	});
 	it('tokens is an array', function () {
-		assert.equal(Array.isArray(instance.output[0].tokens),true);
+		assert.equal(Array.isArray(instance.result[0].tokens),true);
 	});
 	it('tags is an array', function () {
-		assert.equal(Array.isArray(instance.output[0].tags),true);
+		assert.equal(Array.isArray(instance.result[0].tags),true);
 	});
 	it('tokenizationMeta is an array', function () {
-		assert.equal(Array.isArray(instance.output[0].tokenizationMeta),true);
+		assert.equal(Array.isArray(instance.result[0].tokenizationMeta),true);
 	});
 	it('deps is an array', function () {
-		assert.equal(Array.isArray(instance.output[0].deps),true);
+		assert.equal(Array.isArray(instance.result[0].deps),true);
 	});
 	it('raw is a string', function () {
-		assert.equal(typeof instance.output[0].raw,"string");
+		assert.equal(typeof instance.result[0].raw,"string");
 	});
 });
 
 describe('Extensibility', function () {
 
-	var input = "This is a sentence. And this is another one.";
-	it('Single extension', function () {
-		langr.extend({
-			id:"len",
-			detector:function(){
-				return this.sentences.length;
-			}
+	describe('interceptors', function () {
+		it('intercept', function () {
+			var sentence = "something";
+			fin.intercept((str)=>str+" something else");
+			var instance = new fin(sentence);
+			assert.equal(instance.result[0].tokens.length,3);
 		});
-		var instance = new langr(input);
-		assert.equal(instance.len(),2);
 	});
-	it('Multiple extensions', function () {
-		langr.extend([
-			{
-				id:"a",
+
+	describe('detectors', function () {
+		var input = "This is a sentence. And this is another one.";
+		it('Single extension', function () {
+			fin.extend({
+				id:"len",
 				detector:function(){
 					return this.sentences.length;
 				}
-			},
-			{
-				id:"b",
-				detector:function(){
-					return this.sentences.length;
+			});
+			var instance = new fin(input);
+			assert.equal(instance.len(),3);
+		});
+		it('Multiple extensions', function () {
+			fin.extend([
+				{
+					id:"a",
+					detector:function(){
+						return this.sentences.length;
+					}
+				},
+				{
+					id:"b",
+					detector:function(){
+						return this.sentences.length;
+					}
 				}
-			}
-		]);
-		var instance = new langr(input);
-		assert.equal(instance.a(),2);
-		assert.equal(instance.b(),2);
+			]);
+			var instance = new fin(input);
+			assert.equal(instance.a(),3);
+			assert.equal(instance.b(),3);
+		});
 	});
 });
 
