c3d9418cc83a2d790ff4ca3f8342070f3496a95f
Alex Corvi <alex@arrayy.com>
2017-03-15 04:56:29 +0300

    no namespace + ES6 classes

--- src/index.ts
+++ src/index.ts
@@ -8,168 +8,90 @@ import {ResultNode as DepNode} from "en-parse/dist/index";
 import {NodeInterface as TreeInterface} from "en-parse/dist/index";
 import {LexiconType as LexiconExtension} from "en-lexicon/dist/lexicon";
 
-export namespace Fin {
+export class Run {
 
-	/**
-	 * ------------------------------------
-	 * Main function
-	 * ------------------------------------
-	**/
-	export function run (input:string):FinReturn{
-
-		let result:FinReturn = {
-			raw:"",
-			intercepted:"",
-			tokens:[[]],
-			tags:[[]],
-			deps:[[]],
-			depsTree:[],
-			confidence:[],
-			sentences:[]
-		};
-		result.raw = input;
+	public raw:string;
+	public intercepted:string;
+	public sentences:{
+		sentence:string;
+		tokens:string[];
+		tags:string[];
+		deps:DepNode[];
+		depsTree:TreeInterface;
+		confidence:number;
+	}[];
+	constructor(input:string){
+		this.raw = input;
 
 		/**
-		 * ----------------
 		 * 1: Intercepting inputs
-		 * ----------------
 		**/
-		result.intercepted = input;
-		interceptors.forEach((interceptor)=>{
-			result.intercepted = interceptor(result.intercepted);
-		});
-
-		/**
-		 * ----------------
-		 * 2: Tokenization (Lexer)
-		 * ----------------
-		**/
-		const _lexer = new lexed.Lexed(result.intercepted).lexer();
-		result.sentences = _lexer.sentences;
-		result.tokens = _lexer.tokens;
-
-		/**
-		 * ----------------
-		 * 3: POS tagging
-		 * ----------------
-		**/
-		for (var index = 0; index < result.tokens.length; index++) {
-			const tokens = result.tokens[index];
-			const taggingInstance = new tagger.Tag(tokens);
-			taggingInstance.initial();
-			taggingInstance.smooth();
-			result.tags[index] = taggingInstance.tags;
-			result.confidence[index] = taggingInstance.confidence.reduce((a,b)=>a+b,0) / taggingInstance.confidence.length;
-			// but we can never be so sure about the dependency parsing :'(
-			result.confidence[index] = result.confidence[index] - 10;
+		this.intercepted = input;
+		for (var index = 0; index < interceptors.length; index++) {
+			var interceptor = interceptors[index];
+			this.intercepted = interceptor(this.intercepted);
 		}
 
 		/**
-		 * ----------------
-		 * C: Dependency Parsing
-		 * ----------------
+		 * 2: Do the magic
 		**/
-		for (var index = 0; index < result.tokens.length; index++) {
-			const tokens = result.tokens[index];
-			const tags = result.tags[index];
-			result.depsTree[index] = parser.tree(tags,tokens)[0];
-			result.deps[index] = parser.toArray(result.depsTree[index]);
+		const _lexer = new lexed.Lexed(this.intercepted).lexer();
+		for (var index = 0; index < _lexer.sentences.length; index++) {
+			this.sentences[index].sentence = _lexer.sentences[index];
+			this.sentences[index].tokens = _lexer.tokens[index];
+			const taggingInstance = new tagger.Tag(this.sentences[index].tokens).initial().smooth();
+			this.sentences[index].tokens = taggingInstance.tokens;
+			this.sentences[index].confidence = (taggingInstance.confidence.reduce((a,b)=>a+b,0) / taggingInstance.confidence.length) - 10;
+			this.sentences[index].depsTree = parser.tree(this.sentences[index].tags,this.sentences[index].tokens)[0];
+			this.sentences[index].deps = parser.toArray(this.sentences[index].depsTree);
 		}
-
-		/**
-		 * ----------------
-		 * D: Apply Detectors
-		 * ----------------
-		**/
-		detectors.forEach((detector)=>{
-			result = detector(result);
-		});
-		
-		return result;
-	}
-
-	/**
-	 * ------------------------------------
-	 * Extensions directory
-	 * ------------------------------------
-	**/
-	// interceptor functions array
-	export const interceptors:Array<Interceptor> = [];
-	export const detectors:Array<Detector> = [];
-	export function addDetector(detector:Detector|Detector[]){
-		if(!Array.isArray(detector)) detector = [detector];
-		detectors.unshift.apply(detectors,detector);
-	}
-	export function addInterceptor(interceptor:Interceptor|Interceptor[]){
-		if(!Array.isArray(interceptor)) interceptor = [interceptor];
-		interceptors.unshift.apply(interceptors,interceptor);
-	}
-	export function extend(extensions:Extensions){
-		extensions.forEach((extension)=>{
-			if(extension.type === "detector")
-				addDetector(extension.extension);
-			else if(extension.type === "interceptor")
-				addInterceptor(extension.extension);
-			else if(extension.type === "lexicon")
-				extendLexicon(extension.extension);
-			else if(extension.type === "abbreviations") {
-				abbreviations.push.apply(extension.extension);
-			}
-			else {
-				console.warn("FIN WARNING: invalid extension");
-				console.warn(extensions);
-			}
-		});
-	}
-
-	/**
-	 * ------------------------------------
-	 * Interfaces
-	 * ------------------------------------
-	**/
-	export interface Interceptor {
-		(input:string):string;
-	}
-	export interface Detector {
-		(input:FinReturn):FinReturn;
-	}
-	export interface ExtensionDetector {
-		type:"detector";
-		extension:Detector;
-	}
-	export interface ExtensionInterceptor {
-		type:"interceptor";
-		extension:Interceptor;
-	}
-	export interface ExtensionAbbreviations {
-		type:"abbreviations";
-		extension:string[];
-	}
-	export interface ExtensionLexicon {
-		type:"lexicon";
-		extension:LexiconExtension;
-	}
-	export type Extensions = Array <
-		ExtensionDetector|
-		ExtensionInterceptor|
-		ExtensionAbbreviations|
-		ExtensionLexicon
-	>;
-
-	export interface FinReturn {
-		// input strings
-		// raw
-		raw:string;
-		// intercepted
-		intercepted:string;
-		// public (results)
-		tokens:string[][];
-		sentences:string[];
-		tags:string[][];
-		confidence:number[];
-		deps:DepNode[][];
-		depsTree:TreeInterface[];
+		return this;
 	}
 }
 
-export default Fin;
+/**
+ * Extensions directory
+**/
+// interceptor functions array
+export const interceptors:Array<Interceptor> = [];
+export function addInterceptor(interceptor:Interceptor|Interceptor[]){
+	if(!Array.isArray(interceptor)) interceptor = [interceptor];
+	interceptors.unshift.apply(interceptors,interceptor);
+}
+export function extend(extensions:Extensions){
+	extensions.forEach((extension)=>{
+		if(extension.type === "interceptor")
+			addInterceptor(extension.extension);
+		else if(extension.type === "lexicon")
+			extendLexicon(extension.extension);
+		else if(extension.type === "abbreviations") {
+			abbreviations.push.apply(extension.extension);
+		}
+		else {
+			console.warn("FIN WARNING: invalid extension");
+			console.warn(extensions);
+		}
+	});
+}
+
+
+export interface Interceptor {
+	(input:string):string;
+}
+export interface ExtensionInterceptor {
+	type:"interceptor";
+	extension:Interceptor;
+}
+export interface ExtensionAbbreviations {
+	type:"abbreviations";
+	extension:string[];
+}
+export interface ExtensionLexicon {
+	type:"lexicon";
+	extension:LexiconExtension;
+}
+export type Extensions = Array <
+	ExtensionInterceptor|
+	ExtensionAbbreviations|
+	ExtensionLexicon
+>;
